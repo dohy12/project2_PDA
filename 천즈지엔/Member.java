445267bbdc4a8784c9e50365be462cbe0275dd09
chenzijian0 <38966354+chenzijian0@users.noreply.github.com>
2021-04-01 19:04:37 +0900

    part2 http 요청 코드

---
+++ 천즈지엔/Member.java
@@ -0,0 +1,11 @@
+package com.example.pj;
+
+public class Member
+{
+    String name;
+    String phoneNum;
+    String email;
+    String introduction;
+    String kakaoId;
+    String profileLink;
+}
