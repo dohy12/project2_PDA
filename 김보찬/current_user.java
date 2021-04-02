dafcf1fe2c9bb42ab1c6d042d71e34b7f3aeafe0
Bochan kim <44803830+bo1030@users.noreply.github.com>
2021-04-02 22:40:19 +0900

    Update current_user.java

--- 김보찬/current_user.java
+++ 김보찬/current_user.java
@@ -3,44 +3,32 @@ public class current_user{
     private String name;
     private String telnum;
     private String email;
+    private String group;
+    private boolean admin;
 
-    public void logout()
+
+    public boolean is_admin()
     {
-        this.ID = null;
-        this.name = null;
-        this.telnum = null;
-        this.email = null;
+        return admin;
+    } 
+
+    public String getgroup() {
+        return group;
     }
 
     public String getID() {
         return ID;
     }
 
-    public void setID(String ID) {
-        this.ID = ID;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getTelnum() {
         return telnum;
     }
 
-    public void setTelnum(String telnum) {
-        this.telnum = telnum;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
