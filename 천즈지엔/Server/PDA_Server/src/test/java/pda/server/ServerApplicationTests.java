a3bb4475a8cab5b7443db8d40c6e1e2db8a4984e
chenzijian0 <38966354+chenzijian0@users.noreply.github.com>
2021-04-04 04:46:55 +0900

    데이터베이스 연결 암호를 암호화하다
    설정 파일의 데이터베이스 연결 암호를 jasypt로 암호화합니다

--- 천즈지엔/Server/PDA_Server/src/test/java/pda/server/ServerApplicationTests.java
+++ 천즈지엔/Server/PDA_Server/src/test/java/pda/server/ServerApplicationTests.java
@@ -1,16 +1,46 @@
 package pda.server;
 
+import org.jasypt.util.text.BasicTextEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 
 @SpringBootTest
 class ServerApplicationTests
 {
-    @Test
-    void contextLoads()
-    {
+    @Value("${spring.datasource.password}")
+    private String PW;
+    @Value("${jasypt.encryptor.password}")
+    private String KEY;
 
+    @Test
+    void pw()
+    {
+        System.out.println(PW);
+        System.out.println(KEY);
+        // 创建加密对象，默认 PBEWithMD5AndDES
+        BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+        // 加密所需的密钥 6iJ2Qaih28JypTElPmiRgQ==
+        textEncryptor.setPassword("PDA_Server");
+        // 加密后的数据（数据库的用户名或密码）
+        String encData = textEncryptor.encrypt(PW);
+        // 解密后的数据（原数据）
+        String decData = textEncryptor.decrypt(encData);
+        System.out.println("encData: " + encData);
+        System.out.println("decData: " + decData);
     }
 
+    @Value("${spring.datasource.password}")
+    String mysqlPW;
+    @Autowired
+    Environment environment;
+
+    @Test
+    void getSetting()
+    {
+        System.out.println(mysqlPW);
+    }
 }
+
