1ce3e40d58c64736620497c41fbe26db0a3492b9
Bochan kim <44803830+bo1030@users.noreply.github.com>
2021-04-04 20:25:55 +0900

    move server directory

---
+++ Server/PDA_Server/src/main/java/pda/server/Controller/Login.java
@@ -0,0 +1,13 @@
+package pda.server.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class Login {
+
+
+}
