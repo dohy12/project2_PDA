b7146a60bdd9e5ff4b7694f66a2106f0b150f501
Bochan kim <44803830+bo1030@users.noreply.github.com>
2021-04-04 21:09:45 +0900

    로그인 서버 수정
     메소드 추가

--- Server/PDA_Server/src/main/java/pda/server/Controller/Login.java
+++ Server/PDA_Server/src/main/java/pda/server/Controller/Login.java
@@ -1,13 +1,40 @@
 package pda.server.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/login")
 public class Login {
 
+    @RequestMapping(value = "/{group}", method = RequestMethod.GET)
+    public boolean Login(@PathVariable String group, @RequestParam String id, @RequestParam String pw)
+    {
+
+        return result;
+    }
+
+    @RequestMapping(value = "/{group}", method = RequestMethod.POST)
+    public boolean UserCreate(@PathVariable String group)
+    {
+
+        return result;
+    }
+
+    private String[] LoadPW(String id)
+    {
+        return result;
+    }
+
+    private boolean AddUser()
+    {
+        return result;
+    }
+
+    private String HashingF(String pw, String salt)
+    {
+
+        return result;
+    }
 
 }
