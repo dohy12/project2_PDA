173990e6f5f315e2bd41d93f1facbca7230b6f11
Bochan kim <44803830+bo1030@users.noreply.github.com>
2021-04-08 17:26:22 +0900

    Update Login.java
    미구현 상태인 메소드 주석 처리

--- Server/PDA_Server/src/main/java/pda/server/Controller/Login.java
+++ Server/PDA_Server/src/main/java/pda/server/Controller/Login.java
@@ -6,35 +6,34 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/login")
 public class Login {
-
+/*
     @RequestMapping(value = "/{group}", method = RequestMethod.GET)
     public boolean Login(@PathVariable String group, @RequestParam String id, @RequestParam String pw)
     {
 
-        return result;
+        return true;
     }
 
     @RequestMapping(value = "/{group}", method = RequestMethod.POST)
     public boolean UserCreate(@PathVariable String group)
     {
 
-        return result;
+        return true;
     }
 
     private String[] LoadPW(String id)
     {
-        return result;
+        return null;
     }
 
     private boolean AddUser()
     {
-        return result;
+        return true;
     }
 
     private String HashingF(String pw, String salt)
     {
-
-        return result;
+        return null;
     }
-
+*/
 }
