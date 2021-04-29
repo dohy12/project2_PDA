caf9a72991b4a93f24d560e71b8e70cded4b47d1
Bochan kim <44803830+bo1030@users.noreply.github.com>
2021-04-29 23:39:42 +0900

    로그인 기능 추가
    로그인과 JWT 관련 기능 추가

---
+++ Server/PDA_Server/src/main/java/pda/server/DTO/UserPW.java
@@ -0,0 +1,31 @@
+package pda.server.DTO;
+
+public class UserPW {
+    private String U_ID;
+    private String pass_Hash;
+    private String pass_Salt;
+
+    public String getU_ID() {
+        return U_ID;
+    }
+
+    public void setU_ID(String u_ID) {
+        U_ID = u_ID;
+    }
+
+    public String getPass_Hash() {
+        return pass_Hash;
+    }
+
+    public void setPass_Hash(String pass_Hash) {
+        this.pass_Hash = pass_Hash;
+    }
+
+    public String getPass_Salt() {
+        return pass_Salt;
+    }
+
+    public void setPass_Salt(String pass_Salt) {
+        this.pass_Salt = pass_Salt;
+    }
+}
