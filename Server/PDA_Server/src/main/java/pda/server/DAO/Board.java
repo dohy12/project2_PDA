d1bb1c57014c6ffa054025b437b3e38bb6ff4945
CYOUJ <45324921+choiyoujin1201@users.noreply.github.com>
2021-04-30 12:26:23 +0900

    쿼리문 수정
    db 접근 쿼리 수정

--- Server/PDA_Server/src/main/java/pda/server/DAO/Board.java
+++ Server/PDA_Server/src/main/java/pda/server/DAO/Board.java
@@ -4,7 +4,7 @@ import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface Board {
-    @Select("use ${GroupId};" + "select * from board limit 20")
+    @Select("select * from ${GroupId}.board limit 20")
     public void boardList(@Param("GroupId") String GroupId);
     //상위 20개 게시글 읽어오기
 
@@ -12,7 +12,7 @@ public interface Board {
     public void boardPost(@Param("GroupId") String GroupId);
     //게시글 작성하기, 매개변수 타입 수정 필요
 
-    @Delete("use ${GroupId};" + "delete from board where B_ID = ${BID}")
+    @Delete("delete from ${GroupId}.board where B_ID = ${BID}")
     public void boardDelete(@Param("GroupId") String GroupId, @Param("BID") int BID);
     //게시글 삭제하기
 
