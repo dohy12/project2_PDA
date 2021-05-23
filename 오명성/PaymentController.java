f1dbb2b11f2028dd7cbad232a9a83d835ca9f33d
oms801 <oms801@naver.com>
2021-05-23 18:41:52 +0900

    update controller, mapper

--- 오명성/PaymentController.java
+++ 오명성/PaymentController.java
@@ -1,10 +1,10 @@
 package pda.server.Controller;
 
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,104 +22,92 @@ public class PaymentController {
 	} 
 	
 	//단체의 회비 가격, 회비 이름 저장하기 
-	@RequestMapping(path = "/payment/due-infos/", method = RequestMethod.PUT)
+	@RequestMapping(path = "/payment/due-infos", method = RequestMethod.PUT)
     public String select_due_infos(@RequestBody Map<String, Object> param)
     {
-		String GID = (String)param.get("GID");
+		String GroupId = (String)param.get("GroupId");
 		int pay = (int)param.get("pay");
 		String title = (String)param.get("title");
-        paymentMapper.update_due_infos(GID, pay, title);
+        paymentMapper.update_due_infos(GroupId, pay, title);
         return "성공";
     }
 	
 	//회비 가격, 회비 이름 불러오기 
-	@RequestMapping(path = "/payment/due-infos/{GID}", method = RequestMethod.GET)
-	public List select_due_infos(@PathVariable String GID)
+	@RequestMapping(path = "/payment/due-infos/{GroupId}", method = RequestMethod.GET)
+	public Map<String, Object> select_due_infos(@PathVariable String GroupId)
 	{
-		return paymentMapper.select_due_infos(GID);
+		return paymentMapper.select_due_infos(GroupId);
 	}
 	
 	//결제 요청 정보 저장하기
 	@RequestMapping(path = "/payment/request-infos", method = RequestMethod.POST)
-	public String insert_request_infos(@RequestBody Map<String, Object> param)
+	public String insert_request_infos(@RequestBody Map<String, Object> param, @RequestAttribute String U_ID)
 	{
-		String GID = (String) param.get("GID");
-		String user_id = (String) param.get("user_id");
+		String GroupId = (String) param.get("GroupId");
 		String merchant_uid = (String) param.get("merchant_uid");
 		String buyer_name = (String) param.get("buyer_name");
 		String payment_name = (String) param.get("payment_name");
 		int payment_price = (int) param.get("payment_price");
-		paymentMapper.insert_request_infos(GID, user_id, merchant_uid, buyer_name, payment_name, payment_price);
+		paymentMapper.insert_request_infos(GroupId, Integer.parseInt(U_ID), merchant_uid, buyer_name, payment_name, payment_price);
 		return "성공";
 	}
 	
 	//결제 창 설정을 위한 결제 요청 정보 가져오기
-	@RequestMapping(path = "/payment/request-infos/{user_id}", method = RequestMethod.GET)
-	public List select_request_infos(@PathVariable String user_id)
+	@RequestMapping(path = "/payment/request-infos/{GroupId}", method = RequestMethod.GET)
+	public Map<String, Object> select_request_infos(@PathVariable String GroupId, @RequestAttribute String U_ID)
 	{
-		return paymentMapper.select_due_infos(user_id);
+		return paymentMapper.select_request_infos(GroupId, Integer.parseInt(U_ID));
 	}
 	
 	//결제 진행하다 취소되거나, 결제 실패한 경우 결제 요청 정보에서 삭제
-	@RequestMapping(path = "/payment/request-infos/{user_id}", method = RequestMethod.DELETE)
-	public String delete_request_infos(@PathVariable String user_id)
+	@RequestMapping(path = "/payment/request-infos", method = RequestMethod.DELETE)
+	public String delete_request_infos(@RequestAttribute String U_ID)
 	{
-		paymentMapper.delete_result_infos(user_id);
+		paymentMapper.delete_result_infos(Integer.parseInt(U_ID));
 		return "성공";
 	}
 	
 	//결제 성공 시 결과 정보 저장하기
 	@RequestMapping(path = "/payment/results", method = RequestMethod.POST)
-    public String insert_result_infos(@RequestBody Map<String, Object> param)
+    public String insert_result_infos(@RequestBody Map<String, Object> param, @RequestAttribute String U_ID)
     {
-		String user_id = (String)param.get("user_id");
 		String imp_uid = (String)param.get("imp_uid");
 		String merchant_uid = (String)param.get("merchant_uid");
 		String time = (String)param.get("time");
-        paymentMapper.insert_result_infos(user_id, imp_uid, merchant_uid, time);
+        paymentMapper.insert_result_infos(Integer.parseInt(U_ID), imp_uid, merchant_uid, time);
         return "성공";
     }
 	
 	//결제 취소(환불)을 위한 결제 시간 정보 불러오기
-	@RequestMapping(path = "/payment/results/{user_id}", method = RequestMethod.GET)
-	public String select_result_time(@PathVariable String user_id)
+	@RequestMapping(path = "/payment/results", method = RequestMethod.GET)
+	public String select_result_time(@RequestAttribute String U_ID)
 	{
-		return paymentMapper.select_result_time(user_id);
+		return paymentMapper.select_result_time(Integer.parseInt(U_ID));
 	}
 	
 	//해당 시간의 결제 취소(환불)을 위한 결제 결과 정보 불러오기
-	@RequestMapping(path = "/payment/results/{user_id}/{time}", method = RequestMethod.GET)
-	public List select_result_time(@PathVariable String user_id, @PathVariable String time)
+	@RequestMapping(path = "/payment/results/{time}", method = RequestMethod.GET)
+	public Map<String, Object> select_result_time(@RequestAttribute String U_ID, @PathVariable String time)
 	{
-		return paymentMapper.select_result_infos(user_id, time);
+		return paymentMapper.select_result_infos(Integer.parseInt(U_ID), time);
 	}
-	
-	//관리자가 초기에 단체 소개 관련 정보 저장하기
-	@RequestMapping(path = "/groups/intros", method = RequestMethod.PUT)
-    public String insert_group_intros(@RequestBody Map<String, Object> param)
-    {
-		String GID = (String)param.get("GID");
-		String contents = (String)param.get("contents");
-		String image_src = (String)param.get("image_src");
-        paymentMapper.insert_group_intros(GID, contents, image_src);
-        return "성공";
-    }
+
 	
 	//단체 소개 관련 정보 불러오기 
-	@RequestMapping(path = "/payment/groups/intros/{GID}", method = RequestMethod.GET)
-	public List select_group_intros(@PathVariable String GID)
+	@RequestMapping(path = "/payment/groups/intros/{GroupId}", method = RequestMethod.GET)
+	public Map<String, Object> select_group_intros(@PathVariable String GroupId)
 	{
-		return paymentMapper.select_group_intros(GID);
+		return paymentMapper.select_group_intros(GroupId);
 	}
 	
 	//단체 소개 관련 정보 수정하기 (메인 스키마, 그룹 테이블)
 	@RequestMapping(path = "/groups/intros", method = RequestMethod.PUT)
 	public String update_group_intros(@RequestBody Map<String, Object> param)
 	{
-		String GID = (String)param.get("GID");
+		String GroupId = (String)param.get("GroupId");
 		String contents = (String)param.get("contents");
 		String image_src = (String)param.get("image_src");
-	    paymentMapper.update_group_intros(GID, contents, image_src);
+	    paymentMapper.update_group_intros(GroupId, contents, image_src);
 	    return "성공";
 	}
 }
--- 오명성/PaymentMapper.java
+++ 오명성/PaymentMapper.java
@@ -1,57 +1,54 @@
 package pda.server.DAO;
 
-import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.*;
 
+@Mapper
 public interface PaymentMapper {
 	//단체의 회비가격, 회비이름 저장하기 (메인 스키마, 그룹테이블)
-	@Update("UPDATE main.group SET (pay, title) = (${pay}, ${title}) WHERE GID=${GID}")
-	public void update_due_infos(@Param("GID") String GID, @Param("pay") int pay, @Param("title") String title);
+	@Update("UPDATE main.groupinfo SET pay=${pay}, title='${title}' WHERE GroupId='${GroupId}'")
+	public void update_due_infos(@Param("GroupId") String GroupId, @Param("pay") int pay, @Param("title") String title);
 	
 	//단체의 회비가격, 회비이름 불러오기 (메인 스키마, 그룹 테이블)
-	@Select("SELECT pay, title FROM main.group where GID=${GID}")
-    public List select_due_infos(@Param("GID") String GID);
+	@Select("SELECT pay, title FROM main.groupinfo where GroupId='${GroupId}'")
+    public Map<String, Object> select_due_infos(@Param("GroupId") String GroupId);
 	
 	
 	//결제 요청 정보 저장하기 (그룹스키마, 결제 요청 테이블)
-	@Insert("INSERT INTO ${GID}.paymentRequest VALUES (${user_id}, ${merchant_uid}, ${buyer_name}, ${payment_name}, ${payment_price})")
-	public void insert_request_infos(@Param("GID") String GID, @Param("user_id") String user_id,
+	@Insert("INSERT INTO ${GroupId}.paymentRequest VALUES (#{user_id}, '${merchant_uid}', '${buyer_name}', '${payment_name}', ${payment_price})")
+	public void insert_request_infos(@Param("GroupId") String GroupId, int user_id,
 									 @Param("merchant_uid") String merchant_uid, @Param("buyer_name") String buyer_name,
 									 @Param("payment_name") String payment_name, @Param("payment_price") int payment_price);		
 	
 	
 	//결제 요청 정보 불러오기 (그룹스키마, 결제 요청 테이블)
-	@Select("SELECT merchant_uid, buyer_name, payment_name, payment_price FROM ${GID}.paymentRequest WHERE user_id=${user_id}")
-	public List select_request_infos(@Param("GID") String GID);
+	@Select("SELECT merchant_uid, buyer_name, payment_name, payment_price FROM ${GroupId}.paymentRequest WHERE user_id=#{user_id}")
+	public Map<String, Object> select_request_infos(@Param("GroupId") String GroupId, int user_id);
 	
 	//결제 요청 정보 삭제하기 (그룹 스키마, 결제 요청 테이블)
-	@Delete("DELETE FROM paymentRequest WHERE user_id=${user_id}")
-	public void delete_result_infos(@Param("user_id") String user_id);
+	@Delete("DELETE FROM paymentRequest WHERE user_id=#{user_id}")
+	public void delete_result_infos(int user_id);
 	
 	
 	//결제 성공 시 결과 정보 저장하기 (그룹 스키마, 결제 결과 테이블)
-	@Insert("INSERT INTO paymentResult VALUES (${user_id}, ${imp_uid}, ${merchant_uid}, ${time}")
-	public void insert_result_infos(@Param("user_id") String user_id, @Param("imp_uid") String imp_uid, @Param("merchant_uid") String merchant_uid, @Param("time") String time);
+	@Insert("INSERT INTO paymentResult VALUES (#{user_id}, '${imp_uid}', '${merchant_uid}', '${time}'")
+	public void insert_result_infos(int user_id, @Param("imp_uid") String imp_uid, @Param("merchant_uid") String merchant_uid, @Param("time") String time);
 	
 	//결제 시간 정보 불러오기 (그룹 스키마, 결제 결과 테이블)
-	@Select("SELECT time FROM paymentResult WHERE user_id = ${user_id}")
-	public String select_result_time(@Param("user_id") String user_id);
+	@Select("SELECT time FROM paymentResult WHERE user_id = #{user_id}")
+	public String select_result_time(int user_id);
 	
 	//해당 시간의 결제 결과 정보 불러오기 (그룹 스키마, 결제 결과 테이블)
-	@Select("SELECT imp_uid, merchant_uid FROM paymentResult WHERE user_id=${user_id} AND time=${time}")
-	public List select_result_infos(@Param("user_id") String user_id, @Param("time") String time);
+	@Select("SELECT imp_uid, merchant_uid FROM paymentResult WHERE user_id=#{user_id} AND time='${time}'")
+	public Map<String, Object> select_result_infos(int user_id, @Param("time") String time);
 	
-	
-	//단체 소개 관련 정보 저장하기 (메인 스키마, 그룹 테이블)
-	@Update("UPDATE main.group SET (contents, image_src) = (${contents}, ${image_src}) WHERE GID=${GID}")
-	public void insert_group_intros(@Param("contents") String contents, @Param("image_src") String image_src, @Param("GID") String GID);
 	
 	//단체 소개 관련 정보 불러오기 (메인 스키마, 그룹 테이블)
-	@Select("SELECT group_name, contents, image_src FROM main.group WHERE GID=${GID}")
-	public List select_group_intros(@Param("GID") String GID);
+	@Select("SELECT group_name, contents, image_src FROM main.groupinfo WHERE GroupId='${GroupId}'")
+	public Map<String, Object> select_group_intros(@Param("GroupId") String GroupId);
 	
 	//단체 소개 관련 정보 수정하기 (메인 스키마, 그룹 테이블)
-	@Update("UPDATE main.group SET (contents, image_src) = (${contents}, ${image_src}) WHERE GID={GID}")
-	public void update_group_intros(@Param("contents") String contents, @Param("image_src") String image_src, @Param("GID") String GID);
+	@Update("UPDATE main.groupinfo SET contents='${contents}', image_src='${image_src}' WHERE GroupId='{GroupId}'")
+	public void update_group_intros(@Param("contents") String contents, @Param("image_src") String image_src, @Param("GroupId") String GroupId);
 }
