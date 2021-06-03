8294db8823669de314c496be435dca592a8833d8
Bochan kim <44803830+bo1030@users.noreply.github.com>
2021-06-03 21:45:23 +0900

    어플 측 로그인 구현

--- PDA/app/src/main/java/com/example/pda/Login.java
+++ PDA/app/src/main/java/com/example/pda/Login.java
@@ -7,32 +7,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
 import org.json.JSONObject;
-import java.io.BufferedReader;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.security.*;
 import java.security.spec.*;
-import java.text.SimpleDateFormat;
 import java.util.Base64;
-import java.util.Date;
+import java.util.Map;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+import okhttp3.FormBody;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Login extends AppCompatActivity {
+    AlertDialog.Builder alert;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login);;
+        alert = new AlertDialog.Builder(this);
     }
 
     public void goSelectGroup(View view){
+        intent = new Intent(this, SelectGroup.class);
         if(((EditText)findViewById(R.id.ID)).getText().toString().length() == 0 || ((EditText)findViewById(R.id.PW)).getText().toString().length() == 0)
         {
             AlertDialog.Builder alert = new AlertDialog.Builder(this);
@@ -40,85 +55,78 @@ public class Login extends AppCompatActivity {
             alert.show();
         }
         else {
-            String JWT = doLogin();
-            if (JWT.length() > 0) {
-                Intent intent = new Intent(this, SelectGroup.class);
-                intent.putExtra("JWT", JWT);
-                startActivity(intent);
-            } else {
-                AlertDialog.Builder alert = new AlertDialog.Builder(this);
-                alert.setMessage("가입하지 않은 아이디이거나, 잘못된 비밀번호입니다.").setPositiveButton("확인", null);
-                alert.show();
-            }
+            String ID = ((EditText)findViewById(R.id.ID)).getText().toString();
+            String PW = ((EditText)findViewById(R.id.PW)).getText().toString();
+            OkHttpClient client = new OkHttpClient();
+
+            HttpUrl httpUrl = new HttpUrl.Builder()
+                    .scheme("http")
+                    .host("10.0.2.2")
+                    .port(8080)
+                    .addPathSegment("auth")
+                    .addQueryParameter("id", ID)
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url(httpUrl)
+                    .addHeader("pw", EncryptRSA(PW))
+                    .build();
+            System.out.println(httpUrl);
+            System.out.println(request.headers().toString());
+            client.newCall(request).enqueue(new okhttp3.Callback() {
+                @Override
+                public void onFailure(@NotNull okhttp3.Call call, @NotNull IOException e) {
+                    System.out.println("실패");
+                }
+
+                @Override
+                public void onResponse(@NotNull okhttp3.Call call, @NotNull okhttp3.Response response) throws IOException {
+                    try {
+                        JSONObject json = new JSONObject(response.body().string());
+                        intent.putExtra("JWT", json.getString("result"));
+                        startActivity(intent);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
         }
     }
 
     public void goRegister(View view){
-        Intent intent = new Intent(this, Register.class);
+        intent = new Intent(this, Register.class);
         startActivity(intent);
     }
 
-    //JWT 토큰 반환
-    public String doLogin()
-    {
-        String ID = ((EditText)findViewById(R.id.ID)).getText().toString();
-        String PW = ((EditText)findViewById(R.id.PW)).getText().toString();
-        HttpURLConnection httpConn = null;
-        String result = "";
-        try {
-            Date dnow = new Date(System.currentTimeMillis());
-            SimpleDateFormat datef = new SimpleDateFormat("ss");
-            URL url = new URL("http://localhost:8080/auth?id=" + ID + "&pw=" + EncryptRSA(datef.format(dnow) + ":" + PW));
-            System.out.println(url.toString());
-            httpConn = (HttpURLConnection) url.openConnection();
-            httpConn.setRequestMethod("GET");
-            int code = httpConn.getResponseCode();
 
-            if (code == 200)
-            {
-                InputStream response = httpConn.getInputStream();
-                InputStreamReader inputStreamReader = new InputStreamReader(response, "UTF-8");
-                BufferedReader bf = new BufferedReader(inputStreamReader);
-                StringBuffer sb = new StringBuffer();
-                String temp;
-                while ((temp = bf.readLine()) != null) {
-                    sb.append(temp);
-                }
-                JSONObject json = new JSONObject(sb.toString());
-                result = json.getString("JWT");
-            }
-        } catch (MalformedURLException e) { // for URL.
-            e.printStackTrace();
-        } catch (IOException e) { // for openConnection().
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (httpConn != null)
-                httpConn.disconnect();
-        }
-
-        return result;
-    }
-
-    public String EncryptRSA(String plain) throws Exception {
-        System.out.println(plain);
+    public String EncryptRSA(String plain) {
         InputStream fis = getResources().openRawResource(R.raw.pdkey);
-        byte[] keyBytes = new byte[fis.available()];
-        fis.read(keyBytes);
-        fis.close();
-
-        X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
-        KeyFactory kf = KeyFactory.getInstance("RSA");
-        System.out.println(kf.generatePublic(spec));
-        Cipher cip = Cipher.getInstance("RSA");
-        cip.init(Cipher.ENCRYPT_MODE, kf.generatePublic(spec));
-
-        System.out.println(plain);
-        byte[] en = cip.doFinal(plain.getBytes());
-        System.out.println(Base64.getEncoder().encodeToString(en));
-        return Base64.getEncoder().encodeToString(en);
+        byte[] keyBytes;
+        try {
+            keyBytes = new byte[fis.available()];
+            fis.read(keyBytes);
+            fis.close();
+            X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            Cipher cip = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cip.init(Cipher.ENCRYPT_MODE, kf.generatePublic(spec));
+            byte[] en = cip.doFinal(plain.getBytes());
+            return Base64.getEncoder().encodeToString(en);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
