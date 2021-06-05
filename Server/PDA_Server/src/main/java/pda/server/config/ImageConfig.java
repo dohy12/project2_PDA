57fc5be08aeb54a4d18b751ba9443aa3e32ec0af
Bochan kim <44803830+bo1030@users.noreply.github.com>
2021-06-05 12:57:35 +0900

    aws 경로 추가

--- Server/PDA_Server/src/main/java/pda/server/config/ImageConfig.java
+++ Server/PDA_Server/src/main/java/pda/server/config/ImageConfig.java
@@ -5,11 +5,11 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+public class ImageConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
-            .addResourceLocations("file:///C:/images/");
+            .addResourceLocations("file:/home/ubuntu/Server/images/");
     }
 }
