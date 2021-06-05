1107ef3963f640090a45672cce5aa69423c35731
dohy12 <dohy12@naver.com>
2021-06-05 15:36:56 +0900

    단체가입추가 onclick 추가

--- PDA/app/src/main/java/com/example/pda/GroupListFragment.java
+++ PDA/app/src/main/java/com/example/pda/GroupListFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -47,6 +48,15 @@ public class GroupListFragment extends DialogFragment {
             container.addView(v);
 
             v.findViewById(R.id.groupListImage).setClipToOutline(true);
+
+            ///
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast myToast = Toast.makeText(inflater.getContext(),"test", Toast.LENGTH_SHORT);
+                    myToast.show();
+                }
+            });
         }
     }
 }
