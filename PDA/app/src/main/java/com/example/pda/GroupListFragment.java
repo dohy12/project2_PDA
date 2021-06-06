0fbe886738a336ab166e5536df9cafc2b31da62b
chenzijian0 <38966354+chenzijian0@users.noreply.github.com>
2021-06-06 10:29:31 +0900

    단체 가입 구현

--- PDA/app/src/main/java/com/example/pda/GroupListFragment.java
+++ PDA/app/src/main/java/com/example/pda/GroupListFragment.java
@@ -5,11 +5,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+
+import com.example.pda.entity.Group;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class GroupListFragment extends DialogFragment {
     private LinearLayout container;
@@ -28,35 +41,113 @@ public class GroupListFragment extends DialogFragment {
         View v = inflater.inflate(R.layout.group_list_fragment, container, false);
         this.container = v.findViewById(R.id.container);
 
-        setSearchButtonOnclick(v.findViewById(R.id.searchButton));
+        setSearchButtonOnclick(v.findViewById(R.id.searchButton), v);
 
         return v;
     }
 
-    private void setSearchButtonOnclick(View searchButton){
+    private void setSearchButtonOnclick(View searchButton, final View v) {
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showList();
+                TextView textView = v.findViewById(R.id.search);
+                try {
+                    getGroupInf(textView.getText().toString());
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
 
-    private void showList(){
-        for(int i=0;i<10;i++){
-            View v = inflater.inflate(R.layout.group_list_fragment_content, null, false);
-            container.addView(v);
-
-            v.findViewById(R.id.groupListImage).setClipToOutline(true);
-
-            ///
-            v.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Toast myToast = Toast.makeText(inflater.getContext(),"test", Toast.LENGTH_SHORT);
-                    myToast.show();
+    private void getGroupInf(final String Search) throws UnsupportedEncodingException {
+        String Host = "http://18.206.18.154:";
+        String port = "8080";
+        String AccessPath = "/groups/";
+        final String url = Host + port + AccessPath + URLEncoder.encode(Search,"utf-8");
+        final OkHttpClient okHttpClient = new OkHttpClient();
+        final Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("JWT", app.getJWT())
+                .build();
+        final Call call = okHttpClient.newCall(request);
+        Runnable networkTask = new Runnable() {
+            //        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Response response = null;
+                try {
+                    response = call.execute();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            });
-        }
+                String string = null;
+                try {
+                    string = response.body().string();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Gson gson = new Gson();
+                Group group = gson.fromJson(string, Group.class);
+                showList(Search,group.getGroupId());
+
+            }
+        };
+        new Thread(networkTask).run();
+    }
+
+    private void showList(String groupName, final String GID) {
+        String GroupId = GID;
+        View v = inflater.inflate(R.layout.group_list_fragment_content, null, false);
+        container.addView(v);
+
+        v.findViewById(R.id.groupListImage).setClipToOutline(true);
+        TextView viewById = v.findViewById(R.id.groupListName);
+        viewById.setText(groupName);
+        ///
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                joinGroup(GID);
+                Toast myToast = Toast.makeText(inflater.getContext(), "test", Toast.LENGTH_SHORT);
+                myToast.show();
+            }
+        });
+    }
+
+    private void joinGroup(String GID)
+    {
+        String Host = "http://18.206.18.154:";
+        String port = "8080";
+        String AccessPath = "/JoinGroup/";
+        final String url = Host + port + AccessPath + GID;
+        final OkHttpClient okHttpClient = new OkHttpClient();
+        final Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("JWT", app.getJWT())
+                .build();
+        final Call call = okHttpClient.newCall(request);
+        Runnable networkTask = new Runnable() {
+            //        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Response response = null;
+                try {
+                    response = call.execute();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String string = null;
+                try {
+                    string = response.body().string();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+        new Thread(networkTask).run();
     }
 }
