e5c98c18ea03c8284019fe0c9e1fb6137f546eb4
dohy12 <dohy12@naver.com>
2021-06-07 04:03:27 +0900

    앨범 기능 복구

--- PDA/app/src/main/java/com/example/pda/Album_info.java
+++ PDA/app/src/main/java/com/example/pda/Album_info.java
@@ -10,6 +10,24 @@ public class Album_info {
     private int U_ID;
     private String album_intro;
 
+    private String image_src;
+    private int image_cnt;
+    private int comment_cnt;
+
+    public Album_info(int a_id, String title, String date, String location, int u_id, String album_intro, String image_src, int image_cnt, int comment_cnt) {
+        this.setA_ID(a_id);
+        this.setU_ID(u_id);
+        this.setTitle(title);
+        this.setDate(date);
+        this.setLocation(location);
+        this.setAlbum_intro(album_intro);
+
+        this.setImage_src(image_src);
+        this.setImage_cnt(image_cnt);
+        this.setComment_cnt(comment_cnt);
+    }
+
+
     public Album_info(int a_id, String title, String date, String location, int u_id, String album_intro) {
         this.setA_ID(a_id);
         this.setU_ID(u_id);
@@ -22,12 +40,12 @@ public class Album_info {
     public String showJsonString(){
         String res =
                 "{\"A_ID\" : "          + getA_ID() +
-                ",\"title\" : \""       + getTitle() + "\"" +
-                ",\"location\" : \""    + getLocation() + "\"" +
-                ",\"date\" : \""        + getDate() + "\"" +
-                ",\"U_ID\" : "          + getU_ID() +
-                ",\"album_intro\" : \"" + getAlbum_intro() + "\"" +
-                "}";
+                        ",\"title\" : \""       + getTitle() + "\"" +
+                        ",\"location\" : \""    + getLocation() + "\"" +
+                        ",\"date\" : \""        + getDate() + "\"" +
+                        ",\"U_ID\" : "          + getU_ID() +
+                        ",\"album_intro\" : \"" + getAlbum_intro() + "\"" +
+                        "}";
 
         return res;
     }
@@ -56,6 +74,18 @@ public class Album_info {
         return album_intro;
     }
 
+    public int getComment_cnt() {
+        return comment_cnt;
+    }
+
+    public int getImage_cnt() {
+        return image_cnt;
+    }
+
+    public String getImage_src() {
+        return image_src;
+    }
+
     public void setA_ID(int a_ID) {
         A_ID = a_ID;
     }
@@ -80,4 +110,15 @@ public class Album_info {
         this.album_intro = album_intro;
     }
 
+    public void setComment_cnt(int comment_cnt) {
+        this.comment_cnt = comment_cnt;
+    }
+
+    public void setImage_cnt(int image_cnt) {
+        this.image_cnt = image_cnt;
+    }
+
+    public void setImage_src(String image_src) {
+        this.image_src = image_src;
+    }
 }
